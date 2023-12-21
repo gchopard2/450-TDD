f95e13a36a18e54ec4ca81487e7e11ed5f708676
Gwenael Chopard <pt22paf@eduvaud.ch>
2023-12-21 09:25:27 +0100

    R: implémentation du test de 8 - 7 = 1

--- tests/test_stringcalculator_substract.py
+++ tests/test_stringcalculator_substract.py
@@ -4,6 +4,7 @@ import pytest
 @pytest.mark.parametrize("mon_param, mon_resultat", [
     ("", 0), # test case 1
     ("5", 5), # test case 2
+    ("8;7", 1), # test case 3
 ])
 def test_substract_plusieursNombres(mon_param, mon_resultat):
     # Arrange
